662a9e02bbb106b67b89da781c2ded2498577ba1
builtbyjosh <pc.refuge@gmail.com>
2022-01-18 22:55:22 -0600

    initial mongoos and express set up

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.20zms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri)
+
+mongoose.connection.on('connected', ()=>{
+  console.log('connected to mongo')
+})
+
+mongoose.connection.on('error', (err)=> {
+  console.log('error', err)
+})
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+})
